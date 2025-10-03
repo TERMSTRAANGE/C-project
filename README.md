6787c4c4f2fa7b9e9f690df87bb7b90a880e3a8f
TERMSTRAANGE <81357005+TERMSTRAANGE@users.noreply.github.com>
2025-10-03 08:21:09 +0000

    Refactored code into functions, added list, add and basic search function. Improved user experience. Added TODO objectives to README.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # C-project
 Repository for project required in ICS0004
+
+## TODO
+### High Priority
+1. Admin edit and delete functionality
+2. Better, case-insensitive, partial match search
+### Low Priority
+1. Admin password
--- main.c
+++ main.c
@@ -1,23 +1,151 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(){
+#define MAX_PRODUCTS 10
+#define MAX_LENGTH 50
+
+char itemList[MAX_PRODUCTS][MAX_LENGTH];
+int productCount = 0;
+
+void clearScreen() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
+void clearDelay() {
+    printf("Press Enter to continue...");
+    while (getchar() != '\n');
+    getchar();
+}
+
+void addProduct(){
+    if (productCount >= MAX_PRODUCTS) {
+        printf("Product list is full.\n");
+        return;
+    }
+    printf("Enter product name: ");
+    scanf("%s", itemList[productCount]);
+    productCount++;
+    printf("Product added successfully.\n");
+}
+
+void listProducts(){
+    if (productCount == 0){
+        printf("No products available.\n");
+        return;
+    }
+    printf("Available Products:\n");
+    for (int i = 0; i < productCount; i++){
+        printf("%d. %s\n", i + 1, itemList[i]);
+    }
+}
+
+void searchProduct(){
+    char search[MAX_LENGTH];
+    printf("Enter search term: ");
+    scanf("%s", search);
+    int found = 0;
+    for(int i = 0; i < productCount; i++){
+        if (strcmp(itemList[i], search) == 0){
+            printf("Product found: %s\n", itemList[i]);
+            return;
+        }
+    }
+    if (!found){
+        printf("Product not found.\n");
+    }
+}
+
+int customerMenu(){
     int choice;
     while(1){
+        clearScreen();
+        printf("Customer Menu\n");
+        printf("1. List Products\n");
+        printf("2. Search Product\n");
+        printf("3. Exit to Main Menu\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice){
+        case 1:
+            listProducts();
+        clearDelay();
+            break;
+        case 2:
+            searchProduct();
+        clearDelay();
+            break;
+        case 3:
+            return 0;
+        default:
+            printf("Invalid choice. Please try again.\n");
+        clearDelay();
+            break;
+        }
+    }
+}
+
+int adminMenu(){
+    int choice;
+    while(1){
+        clearScreen();
+        printf("Administrator Menu\n");
+        printf("1. List Products\n");
+        printf("2. Add Product\n");
+        printf("3. Edit Product \n");
+        printf("4. Delete Product \n");
+        printf("5. Exit to Main Menu\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice){
+        case 1:
+            listProducts();
+        clearDelay();
+            break;
+        case 2:
+            addProduct();
+        clearDelay();
+            break;
+        case 3:
+            printf("Edit Product.\n");
+            printf("Will be implemented in later release.\n");
+        clearDelay();
+            break;
+        case 4:
+            printf("Delete Product.\n");
+            printf("Will be implemented in later release.\n");
+        clearDelay();
+            break;
+        case 5:
+            return 0;
+        default:
+            printf("Invalid choice. Please try again.\n");
+        clearDelay();
+            break;
+        }
+    }
+}
+
+int mainMenu(){
+    int choice;
+    while(1){
+        clearScreen();
         printf("Main Menu\n");
         printf("1. Customer\n");
         printf("2. Administrator\n");
         printf("3. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        switch (choice)
-        {
+        switch (choice){
         case 1:
-            printf("Product Search.\n");
-            printf("Will be implemented in later release.\n");
+            customerMenu();
             break;
         case 2:
-            printf("Administrator Login.\n");
-            printf("Will be implemented in later release.\n");
+            adminMenu();
             break;
         case 3:
             printf("Exiting the program.\n");
@@ -27,4 +155,8 @@ int main(){
             break;
         }
     }
+}
+
+int main(){
+    return mainMenu();
 }
